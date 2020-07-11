4a55debf367eb2859ec2a2838c10f96a7850a72a
Hypermona <mohans8050@gmail.com>
2020-07-11 19:53:36 +0530

    Express serve static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const http = require("http");
 const { read } = require("fs");
+const morgan = require("morgan");
 
 const host = "localhost";
 const port = 3000;
 
 const app = express();
-
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
