bb6a2fa4860b637aeca46b6b09c477507e3cd0b5
Hypermona <mohans8050@gmail.com>
2020-07-12 20:52:58 +0530

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 const dbname = "couresera";
@@ -9,24 +10,35 @@ MongoClient.connect(url, (err, client) => {
   console.log("Connected correctly to the server");
 
   const db = client.db(dbname);
-  const collection = db.collection("dishes");
-  collection.insertOne(
-    { name: "Uthappizza", description: "test" },
-    (err, result) => {
-      assert.equal(err, null);
-      console.log("After Inset :\n");
-      console.log(result.ops);
+  dboper.insertDocument(
+    db,
+    { name: "Vadonut", description: "Test" },
+    "dishes",
+    (result) => {
+      console.log("Insert Document:\n", result.ops);
 
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        console.log("Found:\n");
-        console.log(docs);
+      dboper.findDocuments(db, "dishes", (docs) => {
+        console.log("Found Documents:\n", docs);
 
-        db.dropCollection("dishes", (err, result) => {
-          assert.equal(err, null);
+        dboper.updateDocument(
+          db,
+          { name: "Vadonut" },
+          { description: "Updated Test" },
+          "dishes",
+          (result) => {
+            console.log("Updated Document:\n", result.result);
 
-          client.close();
-        });
+            dboper.findDocuments(db, "dishes", (docs) => {
+              console.log("Found Updated Documents:\n", docs);
+
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
+
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
