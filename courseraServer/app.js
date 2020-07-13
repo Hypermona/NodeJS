5dd796b2de95fa4459a99d17c5cf5d37f42648d7
Hypermona <mohans8050@gmail.com>
2020-07-13 22:58:40 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- courseraServer/app.js
+++ courseraServer/app.js
@@ -10,6 +10,22 @@ var dishRouter = require("./routes/dishesRouter");
 var promotionRouter = require("./routes/promotionsRouter");
 var leaderRouter = require("./routes/leadersRouter");
 
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
 var app = express();
 
 // view engine setup
