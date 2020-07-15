bb7700c0e85ea39d82588d3fdf5622295eec3ef9
Hypermona <mohans8050@gmail.com>
2020-07-15 11:54:06 +0530

    Basic Authentication

--- courseraServer/app.js
+++ courseraServer/app.js
@@ -36,6 +36,31 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("Your not Authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var user = auth[0];
+  var password = auth[1];
+  if (user == "admin" && password == "1234") {
+    next();
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+app.use(auth); //for authentication
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
